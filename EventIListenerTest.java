0e2446b834b911c63b8745d8837d5b2bac1ca11e
Youda <yudahyeon2@gmail.com>
2022-11-11 10:41:00 +0900

    익명 클래스로 작성 예시

--- EventIListenerTest.java
+++ EventIListenerTest.java
@@ -3,31 +3,35 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class EventIListenerTest extends JFrame {
+	JButton btn;
 	EventIListenerTest() {
         setTitle("Action 이벤트 리스너 예제");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
-        JButton btn = new JButton("Action");
-        btn.addActionListener(new MyActionListener());
+        btn = new JButton("Action");
+        //btn.addActionListener(new MyActionListener());
+        btn.addActionListener(new ActionListener() {
+        	//정의와 함께 생성-> 이름 없는 클래스
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 //JButton b = (JButton)e.getSource();
+		        if(btn.getText().equals("Action"))
+		            btn.setText("액션");
+		        else
+		        	btn.setText("Action");
+				
+			}}
+        );
         c.add(btn);
         setSize(250, 120);
         setVisible(true);
 
     }
 	
-	private class MyActionListener implements ActionListener {
-	    public void actionPerformed(ActionEvent e) {
-	        JButton b = (JButton)e.getSource();
-	        if(b.getText().equals("Action"))
-	            b.setText("액션");
-	        else
-	            b.setText("Action");
 
-	    }
-	}
-	
     public static void main(String[] args) {
         new EventIListenerTest();
     }
+    
 }
