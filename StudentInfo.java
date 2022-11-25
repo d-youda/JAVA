cd0e99996c92c23c817dd123994dccc9849a814d
d-youda <85043343+d-youda@users.noreply.github.com>
2022-11-25 14:18:36 +0900

    saveDataToFile 함수 작성

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,5 +1,9 @@
+import java.io.*;
 import java.util.*;
-
+/*
+ * @author 유다현
+ * 파일에 저장하는 코드
+ */
 public class StudentInfo {
 	public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<Student>();
@@ -18,15 +22,74 @@ public class StudentInfo {
         System.out.println(loadObjectFromFile("output.dat"));
 
     }
-    public static void saveDataToFile(ArrayList<Student> students, String fileName)  {...}
-    public static ArrayList<Student> loadDataFromFile(String fileName) {...}
+    public static void saveDataToFile(ArrayList<Student> students, String fileName)  {
+    	//버퍼에서 입력받기
+    	BufferedReader inputStream = null;
+    	PrintWriter outputStream = null;
+    	try {
+    		inputStream = new BufferedReader(new FileReader(fileName));
+			outputStream = new PrintWriter(new FileWriter(fileName));
+			
+			//students에 있는 애들 file로 옮겨 적기
+			for (int i=0; i < students.size(); i++) {
+				Student s = students.get(i);
+				outputStream.println(s);
+			}
+			
+			//열었던 파일 닫아주기
+			outputStream.close();
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  
+    }
+    public static ArrayList<Student> loadDataFromFile(String fileName) {
+    	ArrayList<Student> result=null;
+    	
+    	return result;
 
-    public static void saveObjectToFile(ArrayList<Student> students, String fileName) {...}
-    public static ArrayList<Student> loadObjectFromFile(String fileName) {...}
+    }
+
+    public static void saveObjectToFile(ArrayList<Student> students, String fileName) {
+    	ObjectOutputStream out = null;
+    	try {
+			out = new ObjectOutputStream(new FileOutputStream(fileName));
+			out.writeObject(students);
+			out.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public static ArrayList<Student> loadObjectFromFile(String fileName) {
+    	ArrayList<Student> result = null;
+    	ObjectInputStream in = null;
+    
+			try {
+				in = new ObjectInputStream(new FileInputStream(fileName));
+				result = (ArrayList<Student>)in.readObject();
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+    	return result;
+    }
+
 	}
-}
 
+//super class인 Student 클래스 생성
 class Student{
+	//대학생과 대학원생 공통으로 해당되는 부분 정의
 	String studentNum;
 	String name;
 	String lesson;
@@ -41,18 +104,26 @@ class Student{
 }
 
 class UnderGraduate extends Student {
+	//대학생만 해당하는 부분 정읜
 	String club;
 	public UnderGraduate(String studentNum, String name, String lesson, int age,String club) {
 		super(studentNum,name,lesson,age);
 		this.club = club;
 	}
+	public String toString() {
+		return studentNum+"|"+name+"|"+lesson+"|"+age+"|"+club;
+	}
 }
 class Graduate extends Student{
+	//대학원생만 해당하는 부분 정읜
 	String degree;
 	String major;
 	public Graduate(String studentNum, String name, String lesson, int age,String degree, String major) {
 		super(studentNum,name,lesson,age);
 		this.degree = degree;
 		this.major = major;
+	}
+	public String toString() {
+		return studentNum+"|"+name+"|"+lesson+"|"+age+"|"+"|"+degree+"|"+major;
 	}
 }
